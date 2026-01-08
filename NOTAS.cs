3ab0f1dc5b7f90e79b6020a9374a716e8252e265
Daniel Otaño Jimenez <d.otano.1@estudiantec.cr>
2026-01-07 22:00:25 -0600

    [ADD] Estructura de las notas y cambio de tempo

---
+++ NOTAS.cs
@@ -0,0 +1,67 @@
+public class ReproductorMusical
+{
+    private Dictionary<string, int> Frecuencias;
+    private Dictionary<string, double> Duraciones;
+
+    public ReproductorMusical()
+    {
+        Frecuencias = new Dictionary<string, int>()
+        {
+            {"DO", 262},
+            {"RE", 294},
+            {"MI", 330},
+            {"FA", 349},  
+            {"SOL", 392},
+            {"LA", 440 },
+            {"SI", 494},
+        };
+
+        Duraciones = new Dictionary<string, double>()
+        {
+            {"redonda", 4.0},
+            {"blanca", 2.0},
+            {"negra", 1.0},
+            {"corchea", 0.5},
+            {"semicorchea", 0.25}
+        };
+    }
+
+    public void CambiarTempo(double nuevaDuracion)
+    {
+
+        if(nuevaDuracion < 0.1 || nuevaDuracion > 5.0)
+        {
+            throw new Exception ("El valor debe de ser entre 0.1 y 5.0");
+        }
+
+        Duraciones ["negra"] = nuevaDuracion;
+
+        Duraciones ["redonda"] = nuevaDuracion * 4;
+        Duraciones ["blanca"] = nuevaDuracion * 2;
+        Duraciones ["corchea"] = nuevaDuracion / 2;
+        Duraciones ["semicorchea"] = nuevaDuracion / 4;
+    }
+
+    public int ObtenerFrecuencia(string nota)
+    {
+        notaMayuscula = nota.ToUpper;
+        if (Frecuencias.ContainsKey(notaMayuscula))
+        {
+            return Frecuencias[notaMayuscula] ;
+        }
+
+        throw new Exception("La nota no se encuentra") ;
+    }
+
+    public int ObtenerMs(string figura)
+    {
+        figuraMinuscula = figura.ToLower;
+
+        if (Duraciones.ContainsKey(figuraMinuscula))
+        {
+            return Duraciones[figuraMinuscula] * 1000 ;
+        }
+    }
+
+}
+
